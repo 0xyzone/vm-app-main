2f044dcb4d537e9c45bdfa47035d28e7ed47b0fb
Pratistha Parajuli <parajulipratistha@gmail.com>
2022-10-18 22:40:05 +0545

    migration table

---
+++ database/migrations/2022_10_18_095404_create_item_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('category');
+            $table->integer('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item');
+    }
+}
--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -1,3 +1,46 @@
 <x-layout>
-  
+  <div class="mb-6 flex flex-col text-white">
+    <label for="category" class=" font-bold">
+        Category
+    </label>
+    <form>
+      @csrf
+      <div class="mb-6 mt-5 w-64">
+        <label for="name" class="">
+            Name
+        </label>
+        <input type="text" class="border border-gray-200 rounded p-2 w-full" name="name"
+            value="{{ old('name') }}" autofocus  />
+        @error('name')
+            <p class="text-white text-xs mt-1">{{ $message }}</p>
+        @enderror
+    <div class="mt-3">
+    <label for="category" class="">
+      Category
+  </label>
+    <select name="category" id="category" class="border border-gray-200 rounded p-2  ">
+        @php
+        $categories = [
+            [
+                "name" => "Food",
+            ],
+            [
+                "name" => "Beverage",
+            ],
+            [
+                "name" => "Miscellaneous",
+            ],
+        ]
+        @endphp
+        <option value="" disabled selected hidden >Please Choose an option.</option>
+        @foreach($categories as $category)
+        <option value="{{ $category['name'] }}" @if(old('category') === $category['name'] ) selected @else @endif>{{ $category['name'] }}</option>
+        @endforeach
+    </select>
+  </div>
+  </form>
+    @error('category')
+        <p class="text-white text-xs mt-1">{{ $message }}</p>
+    @enderror
+</div>
 </x-layout>
