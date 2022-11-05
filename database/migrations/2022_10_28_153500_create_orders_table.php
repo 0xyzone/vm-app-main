f2d323efb99eb364bf2e53d7e99c76cff762a83b
Pratistha Parajuli <parajulipratistha@gmail.com>
2022-11-05 23:15:01 +0545

    orders continue

--- database/migrations/2022_10_28_153500_create_orders_table.php
+++ database/migrations/2022_10_28_153500_create_orders_table.php
@@ -15,6 +15,9 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('table');
+            $table->string('customer');
+            $table->string('item');
             $table->timestamps();
         });
     }
--- resources/views/orders/add.blade.php
+++ resources/views/orders/add.blade.php
@@ -3,7 +3,7 @@
   <h2 class="text-2xl font-bold text-white uppercase text-center my-5">
     Add new Order
 </h2></div>
-<form>
+<form method="POST" action="/orders/store">
   @csrf
   <div class="mb-6">
     <label for="table" class="text-white">
@@ -17,12 +17,32 @@
       @endforeach
   </select>
 </div>
+
 <div class="mb-6">
   <label for="customer" class="text-white">
-      choose a customer
+      Choose a customer
   </label>
- 
+  <select name="customer" id="customer" class="border border-gray-200 rounded p-2 w-full">
+    <option value="" disabled selected hidden>Please Choose an option.</option>
+    @foreach ($customers as $customer)
+        <option value="{{ $customer['id'] }}" @if (old('customer') === $customer['id']) selected @else @endif>
+            {{ $customer['name'] }}</option>
+    @endforeach
 </select>
+</div>
+
+<div class="mb-6">
+  <label for="item" class="text-white">
+      Choose items
+  </label>
+  
+</div>
+
+<div class="mb-6">
+  <button for="add" type="submit"
+      class="text-white  border border-sky-200 rounded py-2 px-4 mt-4 hover:bg-black">
+      Place Order
+  </button>
 </div>
 </form>
 </x-layout>
